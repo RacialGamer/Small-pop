436e91e4c36767f38f9bcbe95842f1c19e1a4cdb
RacialGamer Mods <121128992+RacialGamer2@users.noreply.github.com>
2024-03-31 19:50:29 +0000

    Version: 1.20-0.4

--- src/main/java/net/racialgamer/smallpop/mixin/GameRendererMixin.java
+++ src/main/java/net/racialgamer/smallpop/mixin/GameRendererMixin.java
@@ -1,17 +1,53 @@
 package net.racialgamer.smallpop.mixin;
 
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.item.ItemStack;
 import net.racialgamer.smallpop.config.Gui;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 
+    @Shadow
+    private ItemStack floatingItem;
+
+    @Shadow
+    private int floatingItemTimeLeft;
+
+    @Shadow
+    private float floatingItemWidth;
+
+    @Shadow
+    private float floatingItemHeight;
+
+    @Inject(method = "showFloatingItem", at = @At("TAIL"))
+    public void InjectShowFloatingItem(ItemStack floatingItem, CallbackInfo ci) {
+        this.floatingItem = floatingItem;
+        if(Gui.get().disableTotemPopAnimation) {
+            this.floatingItemTimeLeft = 0;
+        } else {
+            this.floatingItemTimeLeft = Gui.get().animationSpeed;
+        }
+        if (Gui.get().enableTotemPopPositionChange) {
+           this.floatingItemWidth = Gui.get().totemPopPositionX;
+           this.floatingItemHeight = Gui.get().totemPopPositionY;
+        }
+    }
+
     @ModifyVariable(method = "renderFloatingItem", at = @At("STORE"), ordinal = 8)
     private float modifyRenderfloatingItem(float n) {
-        return n * Gui.get().popSize;
+        if (Gui.get().enableTotemPopSizeChange) {
+            float sizeRange = Gui.get().maxTotemPopSize - Gui.get().minTotemPopSize;
+            float size = (float) (Math.sin(System.currentTimeMillis() / 1000.0 * Gui.get().totemPopSizeChangeSpeed) / 2 + 0.5) * sizeRange + Gui.get().minTotemPopSize;
+            return n * size;
+        } else {
+            return n * Gui.get().popSize;
+        }
     }
 }
 
--- src/main/java/net/racialgamer/smallpop/mixin/HeldItemRenderMixin.java
+++ src/main/java/net/racialgamer/smallpop/mixin/HeldItemRenderMixin.java
@@ -7,11 +7,11 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
+import net.racialgamer.smallpop.config.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.racialgamer.smallpop.config.Gui;
 
 
 @Mixin(HeldItemRenderer.class)
@@ -19,10 +19,14 @@ public abstract class HeldItemRenderMixin {
 
     @Inject(method = "renderFirstPersonItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"))
     private void renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
-        if (hand == Hand.MAIN_HAND && item.getItem() == Items.TOTEM_OF_UNDYING) {
-            matrices.scale(Gui.get().totemSize, Gui.get().totemSize, Gui.get().totemSize);
-        } else if (hand == Hand.OFF_HAND && item.getItem() == Items.TOTEM_OF_UNDYING) {
-            matrices.scale(Gui.get().totemSize, Gui.get().totemSize, Gui.get().totemSize);
+            if (Gui.get().enableTotemSizeChange && item.getItem() == Items.TOTEM_OF_UNDYING) {
+                float sizeRange = Gui.get().maxTotemSize - Gui.get().minTotemSize;
+                float size = (float) (Math.sin(System.currentTimeMillis() / 1000.0 * Gui.get().totemSizeChangeSpeed) / 2 + 0.5) * sizeRange + Gui.get().minTotemSize;
+                matrices.scale(size, size, size);
+            } else if (hand == Hand.MAIN_HAND && item.getItem() == Items.TOTEM_OF_UNDYING) {
+                matrices.scale(Gui.get().totemSize, Gui.get().totemSize, Gui.get().totemSize);
+            } else if (hand == Hand.OFF_HAND && item.getItem() == Items.TOTEM_OF_UNDYING) {
+                matrices.scale(Gui.get().totemSize, Gui.get().totemSize, Gui.get().totemSize);
         }
     }
 }
