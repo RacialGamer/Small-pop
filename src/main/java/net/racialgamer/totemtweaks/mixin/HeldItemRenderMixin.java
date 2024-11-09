d90390e32c32e461a967663e7fa33e65e9f4ccc0
RacialGamer <140088807+racialgamer@users.noreply.github.com>
2024-11-09 16:46:03 +0100

    New target in HeldItemRenderMixin.java

--- src/main/java/net/racialgamer/totemtweaks/mixin/HeldItemRenderMixin.java
+++ src/main/java/net/racialgamer/totemtweaks/mixin/HeldItemRenderMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(HeldItemRenderer.class)
 public abstract class HeldItemRenderMixin {
 
-    @Inject(method = "renderFirstPersonItem", at = @At("")) // TODO FIND TARGET
+    @Inject(method = "renderFirstPersonItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"))
     private void renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
         if (Gui.get().enableTotemSizeChange && item.getItem() == Items.TOTEM_OF_UNDYING) {
             float sizeRange = Gui.get().maxTotemSize - Gui.get().minTotemSize;
